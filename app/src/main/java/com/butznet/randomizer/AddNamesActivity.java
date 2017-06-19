21b81ba58206359eb9e8820a1f0d99dbeb93923a
Thomas Johnsen Ulland <thomas.j.ulland@sixty.no>
2017-06-19 09:25:57 +0200

    First commit

---
+++ app/src/main/java/com/butznet/randomizer/AddNamesActivity.java
@@ -0,0 +1,31 @@
+package com.butznet.randomizer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class AddNamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_names);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        configureBackButton();
+    }
+
+    private void configureBackButton() {
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/butznet/randomizer/MainActivity.java
@@ -0,0 +1,95 @@
+package com.butznet.randomizer;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    public TextView tvScreen;
+    public TextView tvButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int n = 10 + random.nextInt(20);
+                tvScreen = (TextView)findViewById(R.id.onScreenView);
+//                tvScreen = (TextView)findViewById(R.id.buttonTextView);
+                final Handler handler = new Handler();
+
+//                tvScreen.setText("");
+
+                for (int i = 0; i < n; i++ ) {
+                    final int finalI = i;
+                    handler.postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            tvScreen.setText(R.string.answer_yes);
+                            tvScreen.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.colorTextFieldBlack));
+                            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                            if ((finalI % 2) == 0) {
+                                getWindow().getDecorView().setBackgroundColor(Color.RED);
+                                tvScreen.setText(R.string.answer_no);
+                                tvScreen.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.colorTextFieldWhite));
+                            }
+                        }
+                    }, (100 * i + i * i ));
+                }
+            }
+        });
+        configureAddNamesButton();
+    }
+
+    private void configureAddNamesButton() {
+        Button addNamesButton = (Button) findViewById(R.id.addNamesButton);
+        addNamesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddNamesActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
